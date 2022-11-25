dda7f4ccc326af131fb61cc101546b2993f543a8
andym80 <andymenutti@gmail.com>
2022-11-25 19:50:47 -0300

    rename a method recent_posts_last_three(id) at app/models/user.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,12 @@ class Post < ApplicationRecord
   has_many :likes
 
   def update_posts_counter(id)
-		user = User.find(id)
-		user.posts_counter = user.posts.count
-		user.save
+    user = User.find(id)
+    user.posts_counter = user.posts.count
+    user.save
   end
 
   def recent_comments(post_id)
     Comment.where(post_id: post_id).order(created_at: :desc).limit(5)
-	end
+  end
 end
