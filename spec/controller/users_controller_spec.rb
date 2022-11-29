091485895eb6bffe40ebaaede0e65e1f68de5a3e
andym80 <andymenutti@gmail.com>
2022-11-29 19:38:57 -0300

    setup spec files

---
+++ spec/controller/users_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'users_controller'
+
+RSpec.describe UsersController, type: :controller do
+
+	describe "GET #index" do
+		it "returns http success" do
+			get :index
+			expect(response).to have_http_status(:success)
+		end
+	end
+
+	describe "GET #show" do
+		it "returns http success" do
+			get :show
+			expect(response).to have_http_status(:success)
+		end
+	end
+end
