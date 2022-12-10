9000733abe93bfc0704b91401dcddd83e7c170e2
andym80 <andymenutti@gmail.com>
2022-12-10 13:56:55 -0300

    add user_index_spec.rb file required examples

--- spec/features/users/user_index_spec.rb
+++ spec/features/users/user_index_spec.rb
@@ -23,5 +23,31 @@ RSpec.describe 'show all users', type: :feature do
         expect(page.find("#user_id_#{user.id}")).to have_content "Number of posts: #{user.posts_counter}"
       end
     end
+
+  describe 'GET #index' do
+    it 'I can see the username of all other users'
+    expect(page).to have_content('user.name')
+  end
+
+  describre 'GET #index' do
+    it 'I can see the profile picture for each user.'
+    expect(page).to have_content('user.photo')
+  end
+
+  describe 'GET #index' do
+    it 'I can see the number of posts each user has written.'
+    expect(page).to have_content('user.posts_counter')
+  end
+
+  describe 'GET #index' do
+    it 'I can see the title of each post.'
+    expect(page).to have_content('post.title')
+  end
+
+  describe 'GET #index' do
+    it 'that redirects to the users show page when you click on a user'
+    expect(page).to have_content('user_path')
+  end
+
   end
 end
